578bc52e6698b05d67a7303093fd6e3c30360eaf
Gregor Weber <gregor.weber@antevorte.org>
2013-07-31 20:44:24 +0200

    Minor enhancements

--- src/message_controller.js
+++ src/message_controller.js
@@ -1,56 +1,56 @@
 var actions = {
-	multiply: function(data) {
-		return {r: data.numbers.reduce(function(p, c) {
-			return p * c;
-		}, 1)};
+		multiply: function(data) {
+			return {r: data.numbers.reduce(function(p, c) {
+				return p * c;
+			}, 1)};
+		},
+		echo: function(params) {
+			return params;
+		}
 	},
-	echo: function(params) {
-		return params;
+
+	Error = {
+		"JSON": 1,
+		"MISSING_ACTION": 2,
+		"INVALID_ACTION": 3,
+		"INVALID_PARAMS": 4
+	},
+
+	parseRequest = function(data) {
+		var action, actionName = data.action;
+		if (!actionName) return Error.MISSING_ACTION;
+
+		action = actions[actionName];
+		if (!action || action.constructor.prototype[actionName]) return Error.INVALID_ACTION;
+
+		delete data.action;
+		return action(data);
 	}
-};
 
 exports.init = function(ws) {
 };
-
-var E = {
-	"MISSING_ACTION": 0,
-	"INVALID_ACTION": 1,
-	"INVALID_PARAMS": 2,
-	"JSON": 3
-};
-function parseRequest(data) {
-	var action, actionName = data.action;
-	if (!actionName) return E.MISSING_ACTION;
-
-	action = actions[actionName];
-	if (!action) return E.INVALID_ACTION;
-
-	delete data.action;
-	return action(data);
-}
-
 exports.handleMessage = function(message, callback) {
 	var response;
 	try {
 		var data = JSON.parse(message);
 		response = parseRequest(data);
 	} catch (e) {
-		response = E.JSON;
+		response = Error.JSON;
 	}
 
 	if (!isNaN(response)) {
 		var error;
 		switch (response) {
-			case E.MISSING_ACTION:
+			case Error.MISSING_ACTION:
 				error = "Missing action parameter.";
 				break;
-			case E.INVALID_ACTION:
+			case Error.INVALID_ACTION:
 				error = "Action does not exist.";
 				break;
-			case E.INVALID_PARAMS:
+			case Error.INVALID_PARAMS:
 				error = "Invalid action parameters.";
 				break;
-			case E.JSON:
+			case Error.JSON:
 				error = "JSON parse error.";
 				break;
 		}
