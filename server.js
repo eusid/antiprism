497bcc465be4f4d4a8c1280f22ddde2205bb5466
Gregor Weber <gregor.weber@antevorte.org>
2013-07-31 06:56:57 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var WebSocketServer = require("ws").Server,
+	wss = new WebSocketServer({port: 8080});
+
+wss.on("connection", function(ws) {
+	console.log("new connection");
+	ws.on("message", function(message) {
+		console.log("received: %s", message);
+	});
+	ws.send("something");
+});
