3107dae687aeb57fca459f26fdeede557e783003
Felix Schmidt <felix.schmidt@antevorte.org>
2013-12-17 23:44:36 +0100

    fix: firefox being gay does not know innerText

--- test/client.js
+++ test/client.js
@@ -248,7 +248,7 @@ var utils = {
       var time = (new Date(message.ts)).toLocaleTimeString().split(' ');
       panelHeader.className = "panel panel-heading";
       panelContent.className = "panel panel-body";
-      panelContent.innerText = message.msg;
+      panelContent.textContent = message.msg;
       var urlregex = /(\b(https?):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/ig,
           results = panelContent.innerHTML.match(urlregex);
       for(link in results) {
