44b90ed4e3fae5712d2ce419a062f8685703e481
p0rnfl4k3s <fabian.beterke@gmail.com>
2013-12-21 22:59:43 +0100

    crashfix

--- src/message_controller.js
+++ src/message_controller.js
@@ -126,8 +126,8 @@ var helpers = {
 				.exec(function(err,replies) {
 					if(!replies[0] && !replies[1])
 						return helpers.sendClient({contacts:[]});
-					var contacts = replies[0],
-						requests = replies[1];
+					var contacts = replies[0]||{},
+						requests = replies[1]||{};
 					var ret = {}, users = Object.keys(contacts), usersIndex = users.length;
 					for(var i in users) {
 						storage.redis.multi()
