81a44f9709b1b280cae61b1fb6cb269f39869797
Felix Schmidt <felix.schmidt@antevorte.org>
2014-02-08 17:39:42 +0100

    changed config

--- client/webrtc.js
+++ client/webrtc.js
@@ -31,12 +31,28 @@ var connection = null,
 
 	};
 
-// Create the connection object
-connection = new webkitRTCPeerConnection({
-	iceServers:[
-		{ url:'stun:stun.l.google.com:19302' }
+//change STUN server if you don't trust google for requesting your public ip:port ;)
+//source: apprtc.appspot.com
+var cfg = {
+	'iceServers': [
+		{
+			'url': 'stun:stun.l.google.com:19302'
+		},
+		{
+			'url': 'turn:192.158.29.39:3478?transport=udp',
+			'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+			'username': '28224511:1379330808'
+		},
+		{
+			'url': 'turn:192.158.29.39:3478?transport=tcp',
+			'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+			'username': '28224511:1379330808'
+		}
 	]
-});
+};
+
+// Create the connection object
+connection = new webkitRTCPeerConnection(cfg);
 
 // Try out new ice candidates
 connection.onicecandidate = function(event) {
